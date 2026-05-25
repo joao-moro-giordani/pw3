318289ebed4bf459f3863bf7256559101189cdaa
João Pedro Moro Giordani <jpmorogiordani@gmail.com>
2026-05-25 17:14:12 -0300

    Feature: tabela de notas e controller/rotas '/keep'

---
+++ resources/views/keep/index.blade.php
@@ -0,0 +1,7 @@
+@extends('keep/_base')
+
+@section('conteudo')
+    <p>Bem-vindo ao Little Keep!</p>
+
+    <p><a href="{{ @route('keep.create') }}">Adicionar nota</a></p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\KeepController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function() {
-    return "Hello World!";
-});
+Route::get('/keep', [KeepController::class, 'index'])->name('keep.index');
 
-Route::get('/num/{n}', function($n) {
-    return 'Número: ' . $n;
-});
-
-Route::get('/calc/{n1}/{n2}', function($n1, $n2) {
-    return "Cálculo: " . $n1 + $n2;
-});
+Route::get('/keep/create', [KeepController::class, 'create'])->name('keep.create');
